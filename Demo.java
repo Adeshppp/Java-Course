7962c91d89eb80bcb6477126a632a510f615ff94
Adesh <adeshpadwal21@gmail.com>
2025-01-13 13:54:21 -0500

    shortcut for for loop

--- Demo.java
+++ Demo.java
@@ -1,8 +1,9 @@
 public class Demo {
     public static void main(String[] args) {
-        
-        for(int i=0;i<10;i++){
-            System.out.println(i);
+        int i=0;
+        for(;i<10;){
+            System.out.println(i++);
+            // i++;
         }
     }
 }
