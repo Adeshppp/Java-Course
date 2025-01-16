2eaca6c16b7bc14085f3112baaab3605b220ca08
Adesh <adeshpadwal21@gmail.com>
2025-01-16 18:38:51 -0500

    throws keyword

---
+++ ExceptionHandling/ThrowsKeyword/Demo.java
@@ -0,0 +1,25 @@
+
+class A {
+    public void print() throws ClassNotFoundException {
+        // try {
+        // System.out.println(Class.forName("Demo"));
+        // } catch (ClassNotFoundException e) {
+        // System.out.println("Class not found: " + e);
+        // }
+
+        System.out.println(Class.forName("calc"));
+
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        A a = new A();
+        try {
+            a.print();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found: " + e);
+        }
+
+    }
+}
