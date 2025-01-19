67fe91d687ceb9f3cf9abde1655ea226fd0f9e99
Adesh <adeshpadwal21@gmail.com>
2025-01-19 15:11:00 -0500

    Selection sort

--- DataStructuresAndAlgorithm/SortingTechniques/SortingTechniques/readme.md
+++ DataStructuresAndAlgorithm/SortingTechniques/SortingTechniques/readme.md
@@ -1,4 +1,4 @@
 ## Famous Array Sorting Techniques
 
-1. Bubble sort O(n^2) : compares each element with all elements and swap
-2. 
+1. Bubble Sort O(n^2) : compares each element with all elements and swap
+2. Selection Sort O(n^2) : Finds max or min number and keep it at the end of unsorted array
--- DataStructuresAndAlgorithm/SortingTechniques/SortingTechniques/src/Demo.java
+++ DataStructuresAndAlgorithm/SortingTechniques/SortingTechniques/src/Demo.java
@@ -1,36 +1,59 @@
 public class Demo {
     public static void main(String[] args) {
-        int[] nums = {6,5,2,8,9,4,6,3};
+        int[] nums = {6, 5, 2, 8, 9, 4};
 
 
         System.out.println("Before Sorting");
-        for(int num :nums) System.out.print(num+" ");
+        for (int num : nums) System.out.print(num + " ");
+        System.out.println();
         System.out.println();
 
-        nums = bubbleSort(nums);
+//        nums = bubbleSort(nums);
+        nums = selectionSort(nums);
 
         System.out.println();
         System.out.println("After Sorting");
-        for(int num :nums) System.out.print(num+" ");
+        for (int num : nums) System.out.print(num + " ");
+    }
+
+    public static int[] selectionSort(int[] nums) {
+        System.out.println("Sorting by using Selection Sort");
+        int minIndex = -1;
+        int temp = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                }
+            }
+            temp = nums[minIndex];
+            nums[minIndex] = nums[i];
+            nums[i] = temp;
+            for (int num : nums) System.out.print(num + " ");
+            System.out.println();
+        }
+        return nums;
     }
 
     public static int[] bubbleSort(int[] nums) {
+        System.out.println("Sorting by using BubbleSort Sort");
         int steps = 0;
         int size = nums.length;
         int temp = 0;
-        for(int i=0;i<size;i++){
+        for (int i = 0; i < size; i++) {
 
-            for(int j=0;j<size-1;j++){
+            for (int j = 0; j < size - 1; j++) {
                 steps++;
-                if(nums[j]> nums[j+1]){
+                if (nums[j] > nums[j + 1]) {
                     temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1]=temp;
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
                 }
             }
         }
 
-        System.out.println("Steps in bubble sort is "+steps);
+        System.out.println("Steps in bubble sort is " + steps);
         return nums;
     }
 }
